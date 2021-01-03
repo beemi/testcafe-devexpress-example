23a35a647ae6fa85ce8f4ee498c1daeea740fb26
Raja Beemi <r.beemireddy@global.com>
2021-01-03 14:41:52 +0000

    feat: Added get page title using Clientfunction

--- testcafe/integration/simple.test.js
+++ testcafe/integration/simple.test.js
@@ -1,25 +1,32 @@
 import {screen} from '@testing-library/testcafe';
+import {ClientFunction} from 'testcafe'
 import {constants} from "../utils/constants";
 import faker from 'faker';
 
 require('dotenv').config();
 
-fixture ("DevExpess home page")
+fixture("DevExpess home page")
     .meta("testID", "JIRA-100")
     .meta({author: "Raj Beemi", creationDate: "25/12/2020"})
     .meta({severity: "critical"})
     .meta({smokeTest: true})
-    .page (process.env.BASE_URL)
+    .page(process.env.BASE_URL)
 
 test("Enter developer name and validate thank you", async t => {
 
     const developerName = faker.name.firstName({gender: "male"});
+    // Get the Page URL
+    const getPageURL = ClientFunction(() => window.location.href);
     // Name
     await t
         .setNativeDialogHandler(() => true)
         .typeText(screen.getByTestId("name-input"), developerName)
         .click(screen.getByTestId("populate-button"));
     console.log("Enter developer name as: " + developerName);
+    console.log("Page URL: " + await getPageURL());
+
+    await t
+        .expect(getPageURL()).contains("/testcafe/example/")
 
     // Feature checkbox
     await t
@@ -32,4 +39,6 @@ test("Enter developer name and validate thank you", async t => {
     // Header
     await t
         .expect(screen.getByTestId("thank-you-header").innerText).contains(constants.THANK_YOU)
+        .expect(getPageURL()).contains("/testcafe/example/thank-you.html")
+    console.log("Page URL after Thank you: " + await getPageURL());
 })
